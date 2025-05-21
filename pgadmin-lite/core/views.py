cfcddb28892cc974faad74eb8ab68a92dafb8d84
Tanushree Sharma <66589482+TanuShree952838@users.noreply.github.com>
2025-05-21 04:56:33 +0000

    fixes

--- pgadmin-lite/core/views.py
+++ pgadmin-lite/core/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.db import connection
-from django.http import JsonResponse
-from django.apps import apps
+
 
 # Utility to fetch columns of a table dynamically
 def get_columns(table_name):
